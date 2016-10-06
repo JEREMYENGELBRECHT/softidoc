7edb1b4eb9992f879da16f1cc399712e688abf5d
JEREMYENGELBRECHT <jeremyngelbrecht@gmail.com>
2016-10-06 17:10:32 +0200

    test

--- AngularUnifiedUI/Clientele.Unity/scripts/Application/SoftiDoc.Unity.Controllers.js
+++ AngularUnifiedUI/Clientele.Unity/scripts/Application/SoftiDoc.Unity.Controllers.js
@@ -218,8 +218,6 @@ angular.module('SoftiDoc.Unity.Controllers', ['SoftiDoc.AuthControllers'])
         $scope.currentApplication = ""; // name : searchUrl
         $scope.currentApplicationKey = ""; // name : searchUrl
 
-        $scope.navItems = "";
-
         //$scope.loggedIn = true;
         //var titleBarNavigation = [];
 
