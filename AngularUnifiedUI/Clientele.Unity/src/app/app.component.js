15e666f6e42c66ea505ed47889868fb84d8fce53
JEREMYENGELBRECHT <jeremyngelbrecht@gmail.com>
2017-04-16 16:52:27 +0200

    11111

--- AngularUnifiedUI/Clientele.Unity/src/app/app.component.js
+++ AngularUnifiedUI/Clientele.Unity/src/app/app.component.js
@@ -6,16 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@angular/core");
-let AppComponent = class AppComponent {
-    constructor() {
+var core_1 = require("@angular/core");
+var AppComponent = (function () {
+    function AppComponent() {
         this.name = 'Angular123456789';
     }
-};
+    return AppComponent;
+}());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: `<h1>Hello {{name}}</h1>`,
+        template: "<h1>Hello {{name}}</h1>",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- AngularUnifiedUI/Clientele.Unity/src/app/app.component.spec.js
+++ AngularUnifiedUI/Clientele.Unity/src/app/app.component.spec.js
@@ -1,27 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_component_1 = require("./app.component");
-const testing_1 = require("@angular/core/testing");
-const platform_browser_1 = require("@angular/platform-browser");
+var app_component_1 = require("./app.component");
+var testing_1 = require("@angular/core/testing");
+var platform_browser_1 = require("@angular/platform-browser");
 describe('AppComponent', function () {
-    let de;
-    let comp;
-    let fixture;
-    beforeEach(testing_1.async(() => {
+    var de;
+    var comp;
+    var fixture;
+    beforeEach(testing_1.async(function () {
         testing_1.TestBed.configureTestingModule({
             declarations: [app_component_1.AppComponent]
         })
             .compileComponents();
     }));
-    beforeEach(() => {
+    beforeEach(function () {
         fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
         comp = fixture.componentInstance;
         de = fixture.debugElement.query(platform_browser_1.By.css('h1'));
     });
-    it('should create component', () => expect(comp).toBeDefined());
-    it('should have expected <h1> text', () => {
+    it('should create component', function () { return expect(comp).toBeDefined(); });
+    it('should have expected <h1> text', function () {
         fixture.detectChanges();
-        const h1 = de.nativeElement;
+        var h1 = de.nativeElement;
         expect(h1.innerText).toMatch(/angular/i, '<h1> should say something about "Angular"');
     });
 });
